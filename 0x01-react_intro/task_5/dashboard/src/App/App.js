86dbedb2be7f75a1c28fd3bcb40f989fcb2756be
Agbaje Sodiq <62660929+LIFE-arch@users.noreply.github.com>
2023-01-26 00:10:56 +0100

    init

---
+++ 0x01-react_intro/task_5/dashboard/src/App/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import logo from '../assets/hbnblogo.jpg';
+import './App.css';
+import { getFullYear, getFooterCopy } from '../utils/utils'
+
+
+function App() {
+  return (
+    <div>
+      <div className="App-header">
+        <img src={logo} alt="logo"/>
+        <h1>School dashboard</h1>
+      </div>
+      <div className="App-body"><p>Login to access the full dashboard</p>
+        <label htmlFor="email">Email: </label>
+        <input type="text" id="email"/>
+        <label htmlFor="password"> Password: </label>
+        <input type="text" id="password"/>
+        <button>OK</button>
+      </div>
+      <div className="App-footer"><p>Copyright {getFullYear()} - {getFooterCopy(false)}</p></div>
+    </div>
+
+  );
+}
+
+export default App;
